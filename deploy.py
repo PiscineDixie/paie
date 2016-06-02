e9b6697fbb1dac4ed7fe181bfdccf67b888fd02e
Pierre Belzile <pierre.belzile@gmail.com>
2016-06-01 20:15:52 -0400

    update script deploiement

--- deploy.py
+++ deploy.py
@@ -1,6 +1,6 @@
 #
 # fabric file for deploying
-#   fab -u root -f paieapp/deploy.py -H dixie backup deploy
+#   fab -u root -f deploy.py -H dixie backup deploy
 #
 
 import fabric.api
@@ -10,9 +10,9 @@ def backup():
     fabric.api.run("cp -a /var/www/paieapp /var/www/paieapp-prev")
     
 def deploy():
-    fabric.api.local("rsync -a --copy-links --delete paieapp %s@%s:/var/www/." % (fabric.api.env.user, fabric.api.env.host))
+    fabric.api.local("rsync -a --delete . %s@%s:/var/www/paieapp/." % (fabric.api.env.user, fabric.api.env.host))
     with fabric.api.cd("/var/www/paieapp"):
-      fabric.api.run("bundle install")
+      fabric.api.run("bundle install --path vendor/bundle")
       fabric.api.run("RAILS_ENV=production bundle exec rake db:migrate")
     fabric.api.run("chown -R www-data.www-data /var/www/paieapp")
     fabric.api.run("apache2ctl graceful")
